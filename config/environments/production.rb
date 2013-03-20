dcd3b80f9bf80ec5c67b3d8bbdb767faa93db549
Kalpesh Fulpagare <kalpesh.fulpagare@weboniselab.com>
2013-03-20 17:30:47 +0530

    Added file for precompilation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,7 @@ RailsApp::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
-  config.assets.precompile += ['news_feeds.css', 'bootstrap.js.coffee', 'lists.js', 'rss_links.js', 'timeago.js']
+  config.assets.precompile += ['news_feeds.css', 'bootstrap.js.coffee', 'lists.js', 'rss_links.js', 'timeago.js', 'news_feeds.js']
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
